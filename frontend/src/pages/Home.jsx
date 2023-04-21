ba48599ffbfd7be6169f471f9cb0549d81cc4b6e
pr454th <prasathkarthiban1999@gmail.com>
2023-04-21 21:41:03 +0530

    deployment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -111,6 +111,7 @@ export default function Home() {
                 value={formData.labName}
                 onChange={handleChange}
                 className="outline-none shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg block w-full p-2.5"
+                required
               />
             </div>
             <div>
@@ -126,6 +127,7 @@ export default function Home() {
                 value={formData.studentPerSetup}
                 onChange={handleChange}
                 className="outline-none shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg block w-full p-2.5"
+                required
               />
             </div>
             <div>
@@ -196,6 +198,7 @@ export default function Home() {
                   value={formData.name}
                   onChange={handleChange}
                   className="outline-none shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg block w-full p-2.5"
+                  required
                 />
               </div>
               <div>
@@ -211,6 +214,7 @@ export default function Home() {
                   value={formData.designation}
                   onChange={handleChange}
                   className="outline-none shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg block w-full p-2.5"
+                  required
                 />
               </div>
               <div>
@@ -226,6 +230,7 @@ export default function Home() {
                   value={formData.qualification}
                   onChange={handleChange}
                   className="outline-none shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg block w-full p-2.5"
+                  required
                 />
               </div>
             </div>
